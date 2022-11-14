147da904fb85059c7ed9f62fb7d285ee57c159cc
Victor Momodu <victoramomodu@gmail.com>
2022-11-14 11:03:44 +0100

    Prewritten script demo comments

--- demo_prewritten.py
+++ demo_prewritten.py
@@ -1,14 +1,21 @@
 from scripter import Scripter
 from script_executor import ScriptExecutor
 
-
+# Initialisation of Scripter class
 scripter = Scripter()
 
-print(scripter.list_scripts())
+# List available scripts
+print("List of available scripts:")
+print(scripter.list_scripts()) # should be empty
 
+# Writes command to .txt file
 scripter.prewritten_script_writer()
+
+# Read list of available scripts
+# If one or more is available, select to read
 commands = scripter.prewritten_script_reader()
 
+# Execute commands
 if commands:
     exe = ScriptExecutor(commands)
     exe.run()
