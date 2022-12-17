3896811f1d326364655afa2067c091fba7bbc439
Victor Momodu <victoramomodu@gmail.com>
2022-12-17 21:18:29 +0100

    Formatted utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import pprint
 
+
 def reverse_mapping(dictionary):
     reversed_dict = dict([(v, k) for k, v in dictionary.items()])
     return reversed_dict
@@ -18,13 +19,13 @@ def list_files(folder_path):
 
     return filenames
 
+
 def string_stripper(string, strip_list):
     stripped_str = string
     for s in strip_list:
-        stripped_str = stripped_str.replace(s, '')
+        stripped_str = stripped_str.replace(s, "")
 
     return stripped_str
-
 
 
 def printer(obj):
