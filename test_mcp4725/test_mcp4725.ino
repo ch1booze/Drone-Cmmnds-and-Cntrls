376bc14d766664e6bd3ebc88fb0e66dd21d91075
Victor Momodu <victoramomodu@gmail.com>
2023-02-08 06:29:52 +0100

    Update test_mcp4725.ino

--- test_mcp4725/test_mcp4725.ino
+++ test_mcp4725/test_mcp4725.ino
@@ -6,16 +6,19 @@ MCP4725 MCP(0x62); // 0x62 or 0x63
 
 #define MCP4725_ADDRESS 0x60 // DAC address (depends on the wiring)
 
+int arr[4];
+int channel = 0;
+
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Wire.begin(); // Initialize the I2C bus
 }
 
 // Function to write the voltage to the DAC
-void writeVoltage(float voltage)
+void writeVoltage(int voltage)
 {
-  int dacValue = (voltage / 5) * 4095;
+  int dacValue = (voltage / 33) * 4095;
   byte highByte = dacValue >> 4;
   byte lowByte = dacValue << 4;
   Wire.beginTransmission(MCP4725_ADDRESS);
@@ -25,44 +28,19 @@ void writeVoltage(float voltage)
   Wire.endTransmission();
 }
 
-int channel(int channel_number)
-{
-  while (Serial.available() == 0)
-  {
-  }
-
-  String commands = Serial.readStringUntil("\r");
-  int str_length = commands.length();
-  String command_str = commands.substring(0, str_length - 1);
-
-  for (int i = 0; i < 4; i++)
-  {
-    int check = i * 3;
-    String substr = command_str.substring(check, check + 3);
-    int value = substr.toInt();
-
-    int channel_value;
-
-    if (i == channel_number)
-    {
-      channel_value = value;
-    }
-
-    return channel_value
-  }
-}
-
 void loop()
 {
-  // Example usage: set the output voltage to 2.5V
-  //  while Serial.available(){};
-  //  Serial.readStringUntil("\r")
-  for (int i = 0; i <= 10; i++)
+  if (Serial.available() >= 0)
   {
-    int channel_number = 0; // 0 for Throttle, 1 for Yaw, 2 for Pitch, 3 for Roll
-    int channel_value = channel(channel_number);
-    float voltage = map(channel_value, 0, 400, 0, 33);
-    writeVoltage(0.1 * voltage);
-    delay(5000);
+    for (int i = 0; i < arraySize; i++)
+    {
+      int incoming = Serial.read();
+      int voltage_level = map(incoming, 200, 400, 0, 33);
+
+      if (i == channel)
+      {
+        writeVoltage(voltage_level)
+      }
+    }
   }
 }
